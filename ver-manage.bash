36d92dca270cf4584105db2ca6b30a642e7ccee5
mkdev55 <120705455+mkdev55@users.noreply.github.com>
2023-11-25 15:23:46 +0000

    test

--- ver-manage.bash
+++ ver-manage.bash
@@ -1,5 +1,10 @@
 previus_v=$(docker images --format '{{.Tag}}' cicd-test | head -n 1)
 
+if [ z "$previus_v"] then
+    echo "Variable 't' is null or unset."
+    exit
+fi
+
 next_ver=$( echo "$previus + 0.1" | bc)
 
 echo $next_ver
